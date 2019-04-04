9b2d793fb242b334f16f71f93eddd7b3036e2140
Akeem Shane <awshane420@gmail.com>
2019-04-04 00:17:28 -0400

    Added Question model for database input

---
+++ lQuestion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _418FinalProject.Models
+{
+    public class Question
+    {
+        [Required]
+        [Column(TypeName ="smallint")]
+        public short QuestionNumber { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string QuestionText { get; set; }
+        [Column(TypeName = "varchar(2)")]
+        public string Answer1 { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Answer1Text { get; set; }
+        [Column(TypeName = "varchar(2)")]
+        public string Answer2 { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Answer2Text { get; set; }
+        [Column(TypeName = "varchar(2)")]
+        public string Answer3 { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Answer3Text { get; set; }
+        [Column(TypeName = "varchar(2)")]
+        public string Answer4 { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Answer4Text { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Image { get; set; }
+        [Column(TypeName ="smallint")]
+        public short Category { get; set; }
+
+    }
+}
