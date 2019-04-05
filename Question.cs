2a66474b520b4b056ad3a00c1b5596c9a3f6a518
Akeem Shane <awshane420@gmail.com>
2019-04-04 23:29:58 -0400

    Did some local database testing updated the questions class so that it can be bounded to the database

--- Question.cs
+++ Question.cs
@@ -6,10 +6,14 @@ namespace _418FinalProject.Models
 {
     public class Question
     {
+        //Constructors
+        public Question() 
+        { 
+        }
 
         public Question(short quesionNumber, string questionText, string answer1, string answer1Text, 
             string answer2, string answer2Text, string answer3, string answer3Text, string answer4, 
-            string answer4Text, short category,string img = null) 
+            string answer4Text, short category, string img = null) 
         {
             QuestionNumber = quesionNumber;
             QuestionText = questionText;
@@ -21,14 +25,16 @@ namespace _418FinalProject.Models
             Answer3Text = answer3Text;
             Answer4 = answer4;
             Answer4Text = answer4Text;
-            Image = img;
+            Image = img; //Since the image is an optional file I have set to be NULL if one isn't added
             Category = category;
 
         }
 
+        //Properties
+        [Key]
         [Required]
         [Column(TypeName ="smallint")]
-        public short QuestionNumber { get; set; }
+        public short QuestionNumber { get; set; } //Primary Key
         [Column(TypeName = "varchar(100)")]
         public string QuestionText { get; set; }
         [Column(TypeName = "varchar(2)")]
