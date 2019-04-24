001ee687d77b8bcaeb9eaa49526fc27ebd4deb52
Akeem Shane <awshane420@gmail.com>
2019-04-24 15:49:22 -0400

    Updated code

--- 418FinalProject/Controllers/LoginController.cs
+++ 418FinalProject/Controllers/LoginController.cs
@@ -1,8 +1,8 @@
 ﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using _418FinalProject.Models;
+using System.Data.SqlClient;
+using System;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -25,21 +25,52 @@ namespace _418FinalProject.Controllers
             return View();
         }
 
-
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> UserLogin(LoginViewModel model)
         {
+            /*
             var usr = await _context.Users.FirstOrDefaultAsync(
+            
             u => (u.Username == model.Username) && (u.Password == model.Password));
-
             if (usr == null) 
             {
                 return NotFound(); 
             }
-
+            
             if (usr.Type == true) { return Redirect("/admin/index"); }
+            */
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "testtaker.database.windows.net";
+            builder.UserID = "user";
+            builder.Password = "Password1";
+            builder.InitialCatalog = "TestTaker";
 
-            return Redirect("/nonadmin/index");
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                connection.Open();
+                String sql = "SELECT USER_NAME, PASSWORD, ADMIN FROM dbo.Users WHERE USER_NAME = '" + model.Username + "';";
+
+
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                            if (reader.GetString(0) == model.Username && reader.GetString(1) == model.Password)
+                            {
+                                if (reader.GetBoolean(2) == true) return Redirect("/Admin/index");
+                                else return Redirect("/NonAdmin/Index");
+                            }
+
+                    }   //need to fix this, gives error message for every incorrect row it checks
+                    connection.Close();
+                }
+            }
+            //if (true) { return Redirect("/admin/index"); }
+
+            return NotFound();
         }
     }
 }
